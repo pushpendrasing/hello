4ec19f355c6ee6d0bd255dce867bf69b68b88144
pushpendrasing <41113154+pushpendrasing@users.noreply.github.com>
2018-11-15 17:27:27 +0530

    magic square program

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import java.util.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int temp=sc.nextInt();
+
+        System.out.println("HElllo worlskjdlkjfkld");
+//        System.out.println(Integer.toString(temp,2));
+//        System.out.println(Integer.toString(temp,8));
+//        System.out.println(Integer.toString(temp,16));
+
+    }
+}
